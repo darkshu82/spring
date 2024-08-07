0296f20e9cc5dcaaa2b2a3f5660c6f6ff5f81897
darkshu82 <darkshu@naver.com>
2024-08-07 22:31:06 +0900

    240807hw

--- 27boardJava05/src/main/java/com/tjoeun/mapper/BoardMapper.java
+++
@@ -1,5 +0,0 @@
-package com.tjoeun.mapper;
-
-public interface BoardMapper {
-
-}
--- boardReviewJava05/src/main/java/com/tjoeun/dto/BoardInfoTable.java
+++
@@ -1,9 +0,0 @@
-package com.tjoeun.dto;
-
-import lombok.Data;
-
-@Data
-public class BoardInfoTable {
-	private int board_info_idx;
-	private String board_info_name;
-}
